ed07ff847549d64c9de742b246ecd73e1511d060
akabihno <arsenijs.kabihno@gmail.com>
2024-11-23 19:19:38 +0200

    Add test class

--- src/LandingWebPage.php
+++ src/LandingWebPage.php
@@ -4,14 +4,25 @@ namespace App;
 
 class LandingWebPage
 {
+    private const FREETEXT_MIN_LENGTH = 20;
+    private const FREETEXT_MAX_LENGTH = 1000;
     public function drawHtml(): void
     {
+        $this->drawPageTitle();
         $this->drawTextInputForm();
+    }
+
+    public function drawPageTitle(): void
+    {
+        echo('Input text to detect language');
     }
 
     protected function drawTextInputForm(): void
     {
-        echo('<input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10" />');
+        echo('<input type="text" id="name" name="name" required minlength="'
+            .self::FREETEXT_MIN_LENGTH.'" maxlength="'
+            .self::FREETEXT_MAX_LENGTH.'" size="1000" />'
+        );
     }
 
 }
