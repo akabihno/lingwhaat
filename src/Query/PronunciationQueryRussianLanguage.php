d17060c4349d48b737125d8bbc27b769ff1d7399
akabihno <arsenijs.kabihno@gmail.com>
2025-01-19 13:36:06 +0200

    Test API

--- src/Query/PronunciationQueryRussianLanguage.php
+++ src/Query/PronunciationQueryRussianLanguage.php
@@ -7,7 +7,7 @@ class PronunciationQueryRussianLanguage extends AbstractQuery
     const PROCESSING_LIMIT = 10;
     public function getArticleName(): void
     {
-        $query = 'SELECT name,ts_crated FROM lingwhaat.'.$this->getTable().'WHERE name LIKE "а%" LIMIT '.self::PROCESSING_LIMIT.' ORDER BY ts_crated DESC';
+        $query = 'SELECT name,ts_created FROM lingwhaat.'.$this->getTable().'WHERE name LIKE "а%" ORDER BY ts_created DESC LIMIT '.self::PROCESSING_LIMIT;
 
         $this->connect();
         var_dump($this->fetch($query));
