4aa2748195933890a0e06500b5e71674704f4aeb
akabihno <arsenijs.kabihno@gmail.com>
2025-01-20 22:24:21 +0200

    Get IPA from Wiktionary API b

--- src/Query/PronunciationQueryRussianLanguage.php
+++ src/Query/PronunciationQueryRussianLanguage.php
@@ -4,7 +4,7 @@ namespace App\Query;
 
 class PronunciationQueryRussianLanguage extends AbstractQuery
 {
-    const PROCESSING_LIMIT = 20;
+    const PROCESSING_LIMIT = 5;
     public function getArticleNames(): array
     {
         $query = 'SELECT name,ts_created FROM lingwhaat.'.$this->getBaseTable().' WHERE name LIKE "б%" ORDER BY ts_created ASC LIMIT '.self::PROCESSING_LIMIT;
