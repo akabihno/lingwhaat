a26edc00556c0b9c737dd4f6a76dd412dcc9d19d
akabihno <arsenijs.kabihno@gmail.com>
2025-01-26 15:16:16 +0200

    Add favicon

--- src/LandingWebPage.php
+++ src/LandingWebPage.php
@@ -9,12 +9,22 @@ class LandingWebPage
     private const FREETEXT_SIZE = 100;
     public function drawHtml(): void
     {
+        $this->drawFavicon();
         $this->drawPhpInfo();
         /*
         $this->drawPageTitle();
         $this->drawDelimiter();
         $this->drawTextInputForm();
         */
+    }
+
+    protected function drawFavicon(): void
+    {
+        echo '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">';
+        echo '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">';
+        echo '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">';
+        echo '<link rel="manifest" href="/site.webmanifest">';
+
     }
 
     protected function drawPhpInfo(): void
