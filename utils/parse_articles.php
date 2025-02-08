a6ea752a854fbb89789a29f30c9242f203e83188
akabihno <arsenijs.kabihno@gmail.com>
2025-02-08 19:07:46 +0200

    Parse Polish IPA

--- utils/parse_articles.php
+++ utils/parse_articles.php
@@ -3,11 +3,13 @@
 require 'vendor/autoload.php';
 
 use App\Query\PronunciationQueryLatvianLanguage;
+use App\Query\PronunciationQueryPolishLanguage;
 use App\Query\PronunciationQueryRussianLanguage;
 use App\Service\WiktionaryArticlesIpaParserService;
 
 // $query = new PronunciationQueryRussianLanguage();
-$query = new PronunciationQueryLatvianLanguage();
+//$query = new PronunciationQueryLatvianLanguage();
+$query = new PronunciationQueryPolishLanguage();
 
 $wiktionaryArticlesService = new WiktionaryArticlesIpaParserService($query);
 
