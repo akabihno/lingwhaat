966f5f04202c1ac0ed859508f84504a2714b4e31
akabihno <arsenijs.kabihno@gmail.com>
2025-02-08 16:38:54 +0200

    Parse Latvian IPA

--- src/Service/WiktionaryArticlesCategoriesService.php
+++ src/Service/WiktionaryArticlesCategoriesService.php
@@ -7,12 +7,13 @@ use App\Query\PronunciationQueryLatvianLanguage;
 class WiktionaryArticlesCategoriesService
 {
     const WIKTIONARY_BASE_API_LINK = 'https://en.wiktionary.org/w/api.php';
-    const WIKTIONARY_RESULT_LIMIT = 15000;
+    const WIKTIONARY_RESULT_LIMIT = 500;
 
     public function __construct(protected PronunciationQueryLatvianLanguage $queryLatvianLanguage)
     {
 
     }
+
     public function getArticlesByCategory(): void
     {
         $params = [
@@ -25,19 +26,27 @@ class WiktionaryArticlesCategoriesService
             "cmlimit" => self::WIKTIONARY_RESULT_LIMIT
         ];
 
-        $url = self::WIKTIONARY_BASE_API_LINK . "?" . http_build_query( $params );
+        do {
+            $url = self::WIKTIONARY_BASE_API_LINK . "?" . http_build_query($params);
 
-        $ch = curl_init( $url );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        $output = curl_exec( $ch );
-        curl_close( $ch );
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $output = curl_exec($ch);
+            curl_close($ch);
 
-        $result = json_decode( $output, true );
+            $result = json_decode($output, true);
 
-        foreach($result["query"]["categorymembers"] as $categoryMember) {
-            var_dump($categoryMember["title"]);
-            $this->queryLatvianLanguage->add($categoryMember["title"]);
-        }
+            foreach($result["query"]["categorymembers"] as $categoryMember) {
+                var_dump($categoryMember["title"]);
+                $this->queryLatvianLanguage->add($categoryMember["title"]);
+            }
+
+            if (isset($result["continue"])) {
+                $params = array_merge($params, $result["continue"]);
+            } else {
+                break;
+            }
+        } while (true);
     }
 
 }
