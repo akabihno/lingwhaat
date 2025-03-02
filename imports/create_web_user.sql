d92797592a3561a850fe15c03d68fb1980dca411
akabihno <arsenijs.kabihno@gmail.com>
2025-03-02 15:10:05 +0200

    Update imports

---
+++ imports/create_web_user.sql
@@ -0,0 +1,20 @@
+CREATE USER '${MYSQL_WEB_USER}'@'%' IDENTIFIED BY '${MYSQL_WEB_PASSWORD}';
+
+GRANT SELECT ON lingwhaat.pronunciation_dutch_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_english_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_esu_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_french_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_german_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_greek_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_hebrew_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_latvian_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_polish_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_portuguese_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_romanian_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_russian_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_serbocroatian_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_spanish_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_tagalog_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT ON lingwhaat.pronunciation_ukrainian_language TO '${MYSQL_WEB_USER}'@'%';
+
+FLUSH PRIVILEGES;
