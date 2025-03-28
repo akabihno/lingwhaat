60e6d48ae7a51aec90fc11776fc2c5943505854b
akabihno <arsenijs.kabihno@gmail.com>
2025-03-28 09:13:14 +0200

    Symfony support

--- process.php
+++ process.php
@@ -2,10 +2,11 @@
 
 require 'vendor/autoload.php';
 
+use App\Controller\EsuLanguageController;
 use App\Controller\InputValidationController;
 use App\Service\TransliterationService;
 
-$transliterationService = new TransliterationService();
+$transliterationService = new TransliterationService(new EsuLanguageController());
 $inputValidationController = new InputValidationController();
 
 $input = $inputValidationController->validate($_POST['language_input']);
