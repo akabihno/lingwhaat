b639749f8e1384731b3ce1bad86e4befc03faa3d
akabihno <arsenijs.kabihno@gmail.com>
2025-03-29 09:22:24 +0200

    Symfony support

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController
 {
