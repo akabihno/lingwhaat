b2e097c3b9261acf4dde6f2fa178848434008db3
akabihno <arsenijs.kabihno@gmail.com>
2025-03-29 19:25:41 +0200

    Symfony support

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
 abstract class AbstractController
 {
+    public function __construct()
+    {
+        $dotenv = new Dotenv();
+        $dotenv->loadEnv(dirname(__DIR__).'/.env');
+    }
 
 }
