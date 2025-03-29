84afec359d083ef436544c3218d74019d5287a65
akabihno <arsenijs.kabihno@gmail.com>
2025-03-29 20:15:47 +0200

    Symfony support

--- src/Controller/EsuLanguageController.php
+++ src/Controller/EsuLanguageController.php
@@ -13,21 +13,11 @@ class EsuLanguageController extends AbstractController
     #[Route('/language', name: 'get_language', methods: ['GET', 'POST'])]
     public function getLanguageData(EntityManagerInterface $entityManager): Response
     {
-        $language = new EsuLanguageEntity();
-        $language->setId(1);
-        $language->setName('tengssuun');
-        $language->setIpa('{{IPA|esu|/ˌtɨŋˈsu.un/|[ˌtɨŋˈsuːn];{{hyph|esu|tengs|suun;');
-
-        //$entityManager->persist($language);
-
-        //$entityManager->flush();
 
         $esuLanguageRepository = $entityManager->getRepository(EsuLanguageEntity::class);
         $result = $esuLanguageRepository->findAllOrderedByName();
 
-        echo $result;
-
-        return new Response('Saved new language record with id '.$language->getId());
+        return new Response($_ENV['DATABASE_URL']);
     }
 
 }
