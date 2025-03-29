ed8fd5856b692ffc867eba683bc18114425446eb
akabihno <arsenijs.kabihno@gmail.com>
2025-03-29 20:31:10 +0200

    Symfony support

--- src/Controller/EsuLanguageController.php
+++ src/Controller/EsuLanguageController.php
@@ -7,9 +7,18 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__, 2).'/vendor/autoload.php';
 
 class EsuLanguageController extends AbstractController
 {
+    public function __construct()
+    {
+        $dotenv = new Dotenv();
+        $dotenv->loadEnv(dirname(__DIR__, 2).'/.env');
+    }
+
     #[Route('/language')]
     public function getLanguageData(EntityManagerInterface $entityManager): Response
     {
