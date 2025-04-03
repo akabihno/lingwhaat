f35a80f3fb23a0dbb28abe6986800c591b4a55f3
akabihno <arsenijs.kabihno@gmail.com>
2025-04-03 12:31:02 +0300

    Update LanguageDetectionService

--- src/Service/LanguageDetectionService.php
+++ src/Service/LanguageDetectionService.php
@@ -83,6 +83,7 @@ class LanguageDetectionService
                 if ($chunk->isFirst()) {
                     if ($response->getStatusCode() == 404 || $response->getStatusCode() >= 500) {
                         error_log("Request failed with status " . $response->getStatusCode() . " for URL: " . $response->getInfo('url'));
+                        break;
                     }
                 }
 
