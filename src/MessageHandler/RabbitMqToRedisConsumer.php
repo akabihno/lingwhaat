d35a8b6a21551bbf28f6660fc404f536e0da109d
akabihno <arsenijs.kabihno@gmail.com>
2025-04-19 13:27:09 +0300

    Test RabbitMQ/Redis

--- src/MessageHandler/RabbitMqToRedisConsumer.php
+++ src/MessageHandler/RabbitMqToRedisConsumer.php
@@ -6,25 +6,27 @@ use App\Service\RedisService;
 use Enqueue\Client\TopicSubscriberInterface;
 use Interop\Queue\Message;
 use Interop\Queue\Context;
-use Predis\Client;
+use Interop\Queue\Processor;
 
-class RabbitMqToRedisConsumer implements TopicSubscriberInterface
+class RabbitMqToRedisConsumer implements Processor, TopicSubscriberInterface
 {
     public function __construct(protected RedisService $redis)
     {
     }
 
-    public function __invoke(Message $message): void
+    public function process(Message $message, Context $context): string
     {
         $data = json_decode($message->getBody(), true);
-
         $this->redis->set('message:' . $data['id'], json_encode($data));
+
+        return self::ACK;
     }
 
     public static function getSubscribedTopics(): array
     {
         return ['redis_messages'];
     }
+
 
 
 }
