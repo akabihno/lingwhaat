7bde671a4dad88b83a27288d75c07b3c96a17365
akabihno <arsenijs.kabihno@gmail.com>
2025-05-01 16:55:57 +0300

    Refactor language controllers

--- src/Repository/FrenchLanguageRepository.php
+++ src/Repository/FrenchLanguageRepository.php
@@ -3,12 +3,12 @@
 namespace App\Repository;
 
 use App\Entity\FrenchLanguageEntity;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class FrenchLanguageRepository extends EntityRepository
+class FrenchLanguageRepository extends ServiceEntityRepository
 {
-    public function __construct(Registry $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, FrenchLanguageEntity::class);
     }
--- src/Repository/GermanLanguageRepository.php
+++ src/Repository/GermanLanguageRepository.php
@@ -3,12 +3,12 @@
 namespace App\Repository;
 
 use App\Entity\GermanLanguageEntity;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class GermanLanguageRepository extends EntityRepository
+class GermanLanguageRepository extends ServiceEntityRepository
 {
-    public function __construct(Registry $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, GermanLanguageEntity::class);
     }
