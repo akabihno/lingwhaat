275e3f786f86dcc4c24cdab00674e310d80b0b75
akabihno <arsenijs.kabihno@gmail.com>
2025-05-10 21:07:07 +0300

    Update docs and refactor

--- src/Entity/EstonianLanguageEntity.php
+++ src/Entity/EstonianLanguageEntity.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\EstonianLanguageRepository;
+use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EstonianLanguageRepository::class)]
 #[ORM\Table(name: "pronunciation_estonian_language")]
