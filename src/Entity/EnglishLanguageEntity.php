2596c1761ae3cd1cbcfa342626d8dd22d06efbee
akabihno <arsenijs.kabihno@gmail.com>
2025-05-17 09:37:59 +0300

    English detection support

--- src/Entity/EnglishLanguageEntity.php
+++ src/Entity/EnglishLanguageEntity.php
@@ -2,7 +2,55 @@
 
 namespace App\Entity;
 
+use App\Repository\EnglishLanguageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EnglishLanguageRepository::class)]
+#[ORM\Table(name: "pronunciation_english_language")]
 class EnglishLanguageEntity
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 256)]
+    private string $name;
+
+    #[ORM\Column]
+    private string $ipa;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): EnglishLanguageEntity
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): EnglishLanguageEntity
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getIpa(): string
+    {
+        return $this->ipa;
+    }
+
+    public function setIpa(string $ipa): EnglishLanguageEntity
+    {
+        $this->ipa = $ipa;
+        return $this;
+    }
 
 }
