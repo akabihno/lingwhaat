1328f4967a3659c1e94529b69e6912551009d7ce
akabihno <arsenijs.kabihno@gmail.com>
2025-06-15 15:55:12 +0300

    Transliteration training

--- src/Service/LanguageDetection/LanguageTransliteration/TrainIpaIpaPredictorModelCommand.php
+++ src/Service/LanguageDetection/LanguageTransliteration/TrainIpaIpaPredictorModelCommand.php
@@ -32,6 +32,7 @@ use Phpml\ModelManager;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'ml:train:ipa-predictor')]
