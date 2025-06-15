7f4dcf89e30198d38b8b68bf5c12558046b4d473
akabihno <arsenijs.kabihno@gmail.com>
2025-06-15 16:48:13 +0300

    Transliteration training

--- src/Service/LanguageDetection/LanguageTransliteration/TrainIpaIpaPredictorModelCommand.php
+++ src/Service/LanguageDetection/LanguageTransliteration/TrainIpaIpaPredictorModelCommand.php
@@ -86,7 +86,7 @@ class TrainIpaIpaPredictorModelCommand extends Command
         $lang = $input->getOption('lang');
 
         $wordsArray = [];
-        $this->modelPath = "/src/Models/ipa_predictor_{$lang}.model";
+        $this->modelPath = "src/Models/ipa_predictor_{$lang}.model";
 
         switch ($lang) {
             case LanguageDetectionService::DUTCH_LANGUAGE_CODE:
