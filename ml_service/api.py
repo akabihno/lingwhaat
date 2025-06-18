c812db3fc9f95ead5d1765968cf954ef50f7a66e
akabihno <arsenijs.kabihno@gmail.com>
2025-06-18 12:17:41 +0300

    Python microservice

--- ml_service/api.py
+++ ml_service/api.py
@@ -4,7 +4,8 @@ import shutil
 import os
 import tempfile
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+path_to_src = Path(__file__).parent / "ml_service"
+sys.path.insert(0, str(path_to_src))
 from model import *
 
 app = FastAPI()
