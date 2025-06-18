ab8306263b33c0a0eb991092567991af6f3e9608
akabihno <arsenijs.kabihno@gmail.com>
2025-06-18 13:42:53 +0300

    Python microservice

--- src/Service/LanguageDetection/LanguageTransliteration/TrainIpaPredictorModelCommand.php
+++ src/Service/LanguageDetection/LanguageTransliteration/TrainIpaPredictorModelCommand.php
@@ -204,7 +204,7 @@ class TrainIpaPredictorModelCommand extends Command
             'body' => [
                 'file' => fopen($file->getRealPath(), 'r'),
             ],
-            'timeout' => 0,
+            'timeout' => null,
         ]);
 
         $statusCode = $response->getStatusCode();
