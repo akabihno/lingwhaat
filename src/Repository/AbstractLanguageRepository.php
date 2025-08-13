e7117931f7581d7d0c9e2e06d74c75668f752f36
akabihno <arsenijs.kabihno@gmail.com>
2025-08-13 18:03:07 +0300

    ML adjustments

--- src/Repository/AbstractLanguageRepository.php
+++ src/Repository/AbstractLanguageRepository.php
@@ -7,7 +7,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 abstract class AbstractLanguageRepository extends ServiceEntityRepository
 {
-    const int PRONUNCIATION_MAX_RESULTS = 200000;
+    const int PRONUNCIATION_MAX_RESULTS = 50000;
     public function __construct(ManagerRegistry $registry, $entityClass)
     {
         $dotenv = new Dotenv();
