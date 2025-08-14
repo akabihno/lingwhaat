d6cf158f8941617c2cb2f577d4d4882575e02d35
akabihno <arsenijs.kabihno@gmail.com>
2025-08-14 22:05:55 +0300

    Rate limiter for index page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,14 +10,8 @@ class IndexController extends AbstractController
 {
     const string TEXT_PROMPT = 'Enter text to detect language';
     #[Route('/', name: 'index', methods: ['GET'])]
-    public function index(Request $request, RateLimiterFactory $anonymousApiLimiter): Response
+    public function index(): Response
     {
-        $limiter = $anonymousApiLimiter->create($request->getClientIp());
-
-        if (false === $limiter->consume(1)->isAccepted()) {
-            return $this->render('too_many_requests.html.twig');
-        }
-
         return $this->render('index.html.twig', [
             'prompt' => self::TEXT_PROMPT,
         ]);
