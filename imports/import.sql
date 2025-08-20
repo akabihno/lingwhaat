3e2ade575c2ebfa5c46ad6bdd736e2ddc4d8a8b7
akabihno <arsenijs.kabihno@gmail.com>
2025-08-20 16:38:25 +0300

    Update DB import

--- imports/import.sql
+++ imports/import.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0252ab520eede12c5c69b34167c8584ee88f85c5e4ae3bbc922ce9b2f93a15f9
-size 423859929
+oid sha256:234559b10aee011da0a896a1a91e784fdacfb5d2322bb2fcb3e81236a8aeabfc
+size 425612679
