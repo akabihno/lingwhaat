716bfd11dd8a8e4e392e1c5378d2bf505c0979cf
akabihno <arsenijs.kabihno@gmail.com>
2025-10-12 11:42:21 +0300

    Add API endpoint

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,20 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-require dirname(__DIR__, 2).'/vendor/autoload.php';
-
 class IndexController extends AbstractController
 {
-    public function __construct()
-    {
-        $dotenv = new Dotenv();
-        $dotenv->loadEnv(dirname(__DIR__, 2).'/.env');
-    }
     #[Route('/', name: 'index', methods: ['GET'])]
     public function index(): RedirectResponse
     {
