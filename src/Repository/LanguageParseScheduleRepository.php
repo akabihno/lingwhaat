5c9256fdc1f6019e98b5368a5f2d519e55438809
akabihno <arsenijs.kabihno@gmail.com>
2025-12-14 17:56:50 +0200

    Add transliteration

--- src/Repository/LanguageParseScheduleRepository.php
+++ src/Repository/LanguageParseScheduleRepository.php
@@ -2,18 +2,19 @@
 
 namespace App\Repository;
 
+use App\Entity\LanguageParseScheduleEntity;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Dotenv\Dotenv;
 
 class LanguageParseScheduleRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry, $entityClass)
+    public function __construct(ManagerRegistry $registry)
     {
         $dotenv = new Dotenv();
         $dotenv->loadEnv(dirname(__DIR__, 2).'/.env');
 
-        parent::__construct($registry, $entityClass);
+        parent::__construct($registry, LanguageParseScheduleEntity::class);
     }
 
     public function getAll(): array
