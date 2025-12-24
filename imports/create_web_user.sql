5a03d5e79b899e28fbe5e4ff699403f965cbef0b
akabihno <arsenijs.kabihno@gmail.com>
2025-12-24 15:56:21 +0200

    Add languages

--- imports/create_web_user.sql
+++ imports/create_web_user.sql
@@ -52,4 +52,5 @@ GRANT SELECT,INSERT,UPDATE ON lingwhaat.pronunciation_hausa_language TO '${MYSQL
 GRANT SELECT,INSERT,UPDATE ON lingwhaat.pronunciation_urdu_language TO '${MYSQL_WEB_USER}'@'%';
 GRANT SELECT,INSERT,UPDATE ON lingwhaat.pronunciation_gullah_language TO '${MYSQL_WEB_USER}'@'%';
 GRANT SELECT,INSERT,UPDATE ON lingwhaat.pronunciation_vietnamese_language TO '${MYSQL_WEB_USER}'@'%';
+GRANT SELECT,INSERT,UPDATE ON lingwhaat.pronunciation_mandarin_language TO '${MYSQL_WEB_USER}'@'%';
 FLUSH PRIVILEGES;
