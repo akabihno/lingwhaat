d76967fe99c98829d8b494c21d6dec2e4f849c0b
akabihno <arsenijs.kabihno@gmail.com>
2026-01-30 22:09:14 +0200

    Add index to wikipedia_article table

--- src/Entity/WikipediaArticleEntity.php
+++ src/Entity/WikipediaArticleEntity.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: "wikipedia_article")]
+#[ORM\Index(name: 'i_language_code', columns: ['language_code'])]
 class WikipediaArticleEntity
 {
     #[ORM\Id]
