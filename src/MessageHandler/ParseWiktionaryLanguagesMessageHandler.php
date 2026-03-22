2b9818cc4df4fcb71345c51ef41fd31c1b376b59
akabihno <arsenijs.kabihno@gmail.com>
2026-03-22 14:25:23 +0200

    Automate language adding

--- src/MessageHandler/ParseWiktionaryLanguagesMessageHandler.php
+++ src/MessageHandler/ParseWiktionaryLanguagesMessageHandler.php
@@ -5,6 +5,7 @@ namespace App\MessageHandler;
 use App\Message\ParseWiktionaryArticlesMessage;
 use App\Message\ParseWiktionaryLanguagesMessage;
 use App\Repository\LanguageParseScheduleRepository;
+use App\Service\Logging\ElasticsearchLogger;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -14,12 +15,20 @@ class ParseWiktionaryLanguagesMessageHandler
     public function __construct(
         private readonly LanguageParseScheduleRepository $repository,
         private readonly MessageBusInterface $bus,
+        private readonly ElasticsearchLogger $logger,
     ) {
     }
 
     public function __invoke(ParseWiktionaryLanguagesMessage $message): void
     {
-        foreach ($this->repository->getAll() as $language) {
+        $languages = $this->repository->getAll();
+
+        $this->logger->info(
+            sprintf('Dispatching Wiktionary IPA parsing for %d language(s).', count($languages)),
+            ['service' => '[ParseWiktionaryLanguagesMessageHandler]']
+        );
+
+        foreach ($languages as $language) {
             $this->bus->dispatch(
                 new ParseWiktionaryArticlesMessage($language->getLanguageName(), $message->getLimit())
             );
