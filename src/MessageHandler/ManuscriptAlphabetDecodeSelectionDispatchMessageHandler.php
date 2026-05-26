379e9ec5da454d6a9ee1aa589dd67dacd6427507
akabihno <arsenijs.kabihno@gmail.com>
2026-05-26 10:14:09 +0300

    Decrease limit for src/MessageHandler/ManuscriptAlphabetDecodeSelectionDispatchMessageHandler.php

--- src/MessageHandler/ManuscriptAlphabetDecodeSelectionDispatchMessageHandler.php
+++ src/MessageHandler/ManuscriptAlphabetDecodeSelectionDispatchMessageHandler.php
@@ -13,7 +13,7 @@ use Symfony\Component\Messenger\MessageBusInterface;
 class ManuscriptAlphabetDecodeSelectionDispatchMessageHandler
 {
     private const string LOG_SERVICE = '[ManuscriptAlphabetDecodeSelectionDispatchMessageHandler]';
-    private const int BUDGET_PER_RUN = 3000;
+    private const int BUDGET_PER_RUN = 5;
 
     public function __construct(
         private readonly ManuscriptAlphabetDecodeResultRepository $resultRepository,
